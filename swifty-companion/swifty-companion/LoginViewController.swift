a32a3a2a6d388bbeaea29b3147653210a814739c
Stepan Kirillov <stepakirillov59@gmail.com>
2022-02-22 11:02:07 +0300

    forgot checkout

--- swifty-companion/swifty-companion/LoginViewController.swift
+++ swifty-companion/swifty-companion/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 struct Constants {
 	static let clientId = "b642354b128b0e203de73888e9ddad3456b93fcf168ddbbeaf7f5801d183649b"
@@ -19,13 +20,65 @@ struct Constants {
 	static let redirectURI = "swifty-companion://oauth-callback"
 }
 
-class LoginViewController: UIViewController {
+enum CompleteStatus {
+	case fail
+	case success
+}
 
+class LoginViewController: UIViewController {
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 	}
-
+	
+	func login() {
+		guard let signInURL = createSignInURL() else {
+			print("Can't create signIn URL")
+			return
+		}
+		
+		let authenticationSession = ASWebAuthenticationSession(url: signInURL, callbackURLScheme: Constants.callbackURL) { [weak self] callbackURL, error in
+			guard error == nil,
+				  let callbackURL = callbackURL,
+				  let queryItems = URLComponents(string: callbackURL.absoluteString)?.queryItems,
+				  let code = queryItems.first(where: {$0.name == "code" })?.value,
+				  let codeExchangeURL = self?.createCodeExchangeURL(code: code)
+			else {
+				print("An error occurred when attempting to sign in.")
+				return
+			}
+			
+			
+		}
+	}
+	
+	func netwirking(codeExchangeUrl: URL, completionHandler: @escaping ((CompleteStatus) -> Void)) {
+		var request = URLRequest(url: codeExchangeUrl)
+		
+	}
+	
+	func createCodeExchangeURL(code: String) -> URL?{
+		let queryItems = [
+			URLQueryItem(name: "grant_type", value: "authorization_code"),
+			URLQueryItem(name: "client_id", value: Constants.clientId),
+			URLQueryItem(name: "client_secret", value: Constants.clientSecret),
+			URLQueryItem(name: "code", value: code),
+			URLQueryItem(name: "redirect_uri", value: Constants.redirectURI)
+		]
+		return (createURLWithComponents(host: Constants.apiHost, path: Constants.tokenPath, queryItems: queryItems))
+	}
+	
+	func createSignInURL() -> URL? {
+		let queryItems = [
+			URLQueryItem(name: "client_id", value: Constants.clientId),
+			URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+			URLQueryItem(name: "scope", value: Constants.scope),
+			URLQueryItem(name: "response_type", value: "code")
+		]
+		return (createURLWithComponents(host: Constants.apiHost, path: Constants.authPath, queryItems: queryItems))
+	}
+	
 	func createURLWithComponents(host: String = Constants.host, path: String, queryItems: [URLQueryItem]? = nil) -> URL? {
 		var urlComponents = URLComponents()
 		urlComponents.scheme = "https"
